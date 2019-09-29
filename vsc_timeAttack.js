259b78b2504ff9df5de9fec62120d3d62110d43c
Masayuki Miyake <ming@MasayukinoMacBook-Air.local>
2019-09-30 00:20:32 +0900

    first commit

---
+++ vsc_timeAttack.js
@@ -0,0 +1,193 @@
+// FIRST OF ALL, INSTALL EXTENSION "Insert Date String" from MARKETPLACE!
+// ON STARTING TIME ATTACK, PRESS "ctrl + shift + i" TO SET TIMESTAMP TO VARIABLE 'start'!
+
+// ARE YOU READY?
+
+// GO!
+const start = "";
+
+// Some statements are wrong (contains "wrong") . They should be fixed.
+function fixStatement(){
+  const hint = "I recommend that" +
+    "in Windows, press 'ctrl' + 'd' on wrong statement" +
+    "in Mac, press 'command' + 'd' on wrong statement." +
+    "And press 'escape' after that." +
+    "Or you can use 'command' + 'shift' + 'l' in Mac.";
+  const message = "If you can see blue stars, reduce 3 sec from your time."
+  let milkyWay = "";
+
+  wrongmilkyWay = "。　　　　　 　｡　　*。, +　。.o　ﾟ, 。*, o　。.<br>";
+  wrongmilkyWay = wrongmilkyWay + "ﾟ　　o.。　　　.　　.　　　,　　.　, ｏ　。ﾟ.　, ﾟ　。　＋　。　。, ﾟ.。<br>";
+  wrongmilkyWay = wrongmilkyWay + "ﾟ,　　　, 。　.＋　　ﾟ　　　。　　。ﾟ.ﾟ。, ☆　*　。ﾟ.o.ﾟ　　。　.。<br>";
+  wrongmilkyWay = wrongmilkyWay + "  。　.．。　　　　o　　　．.。　ﾟ　 ﾟ, 。.o　。*　。　.o．　。　．　.<br>";
+  wrongmilkyWay = wrongmilkyWay + "。　　　.。　　．　.ﾟo　。　*．　。　．.☆　.＋.　.　　.<br>";
+  wrongmilkyWay = wrongmilkyWay + "。　.．　.　　　.．　　。　ﾟ。, ☆　ﾟ.＋　。　ﾟ, 。　.。　　,　.。<br>";
+  wrongmilkyWay = wrongmilkyWay + "ﾟ　　。　　　ﾟ.　　+。　ﾟ *　。.　, 。ﾟ +.。*。　ﾟ.　　　.　.．　　.<br>";
+  wrongmilkyWay = wrongmilkyWay + "。　　.　　　.。　。ﾟ.。*　。, ´。.☆。。.ﾟ。+　。　.。　　.｡　　　.<br>";
+  wrongmilkyWay = wrongmilkyWay + "　.。　　ﾟ　ﾟ。　。,　.。o　☆　+　, ﾟ。　*。.。　。　.。　　　　.<br>";
+  wrongmilkyWay = wrongmilkyWay + "ﾟ.ﾟ　ﾟ　　。ﾟ +　。.+。　*　。ﾟ。ﾟ.,　, +　。ﾟ.。　.　.　　　,　　　　,　　　.<br>";
+  wrongmilkyWay = wrongmilkyWay + "ﾟ。ﾟ + ﾟ`,　o。。.ﾟ*。ﾟ　。.ﾟ　。　☆＋。。ﾟ.　°　。　.　　　,　　　　　　ﾟ　　　　ﾟ<br>";
+  wrongmilkyWay = wrongmilkyWay + "。,　.ﾟ。　+　☆。,ﾟ.　o。 。+　。ﾟ.,　　.　ﾟ　　　,　　　。　　　　　。　　　.　　　.<br>";
+  wrongmilkyWay = wrongmilkyWay + "ﾟ.　ｏ　*　。ﾟ。ﾟ.。ﾟ。+ﾟ　。　。　ﾟ。 ﾟ　。　　ﾟ<br>";
+  wrongmilkyWay = wrongmilkyWay + "ﾟ`　.ﾟ.ﾟ.ﾟ.　.ﾟ.ﾟ.　　　,　　.　　　　　.　　.。　　　　　　ﾟ.<br>";
+  wrongmilkyWay = wrongmilkyWay + ".　　.　　　　　.　, 。　　　　　　　.　　　　　　　　　　　.　　,　　　　.<br>";
+  wrongmilkyWay = wrongmilkyWay + "。　　　　　　　　　　　　　　　　　ﾟ.";
+  const color = "";
+  wrongcolor = "#0000ff";
+  const htmlSection1 = document.getElementById("section1");
+  htmlSection1.style.color = color || "";
+  htmlSection1.innerHTML = "<p>" + milkyWay + "<br>" + message + "</p>";
+}
+
+// Find style attributes in commented out line and set to variable 's'.
+function setAttributes(){
+  // supercalifragilisticexpialidocious supercalifragilisticexpialidocious supercalifragilisticexpialidocious supercalifragilisticexpialidocious supercalifragilisticexpialidocious font-size:48px;color:red; supercalifragilisticexpialidocious supercalifragilisticexpialidocious
+  const s = "";
+  const comment = "I recommend that" +
+    "in Windows, press 'ctrl' + right arrow to jump to next word." +
+    "in Mac, press 'option' + right arrow to jump to next word." ;
+  const wordStyle = s=="" ? "12px" : s;
+  const message = "If both 'A' below have the same size and color, reduce 4 sec."
+  const htmlSection2 = document.getElementById("section2");
+  htmlSection2.innerHTML = "<p><span style='" + wordStyle + "'>A</span><br>" +
+    "<span style='" + foo + "'>A</span><br>" + message + "</p>";
+}
+
+// Copy a line from end of file and paste in blank line below.
+// don't forget uncomment the line.
+function copyFromEOF(){
+    // warp
+  const comment = "I recommend that" +
+    "in Windows, press 'ctrl' + 'f' to set 'warp' and 'ctrl' + 'g' to come back to next 'warp' " +
+    "in Mac, press 'command' + 'f' to set 'warp' and 'command' + 'g' to come back to next 'warp'." +
+    "And you can go to end of file with 'ctrl' + 'end' in Windows " +
+    "or use 'command' + down arrow in Mac.";
+  const superCow = "" +
+    "       (__)<br>"+
+    "       (oo)<br>"+
+    // warp
+
+    "/ |    ||  <br>"+
+    "* /\---/\  ";
+  const message = "If you can see cow, reduce 4 sec from your time."
+  const htmlSection3 = document.getElementById("section3");
+  htmlSection3.innerHTML = "<p>" + superCow + "<br>" + message + "</p>";
+}
+
+// IF YOU FINISH THE CHALLENGE, PRESS "ctrl + shift + i" TO SET TIMESTAMP TO VARIABLE 'finish'!
+// IF NOT, SEARCH AND GO TO "alpaca".
+const finish = "";
+
+// Get your alpaca's neck longer more than 3 lines. 
+function alpaca(){
+  const comment = "I recommend that" +
+    "in Windows, press 'ctrl' + 'c' -> 'ctrl' + 'enter' -> 'ctrl' + 'v' without select words," +
+    "in Mac, press 'command' + 'c' -> 'command' + 'enter' -> 'command' + 'v' without select words," +
+    "to copy line and paste it next.";
+  const yourPaca = "" +
+    " 　_　　　　　　　　_<br>" +
+    "　{ ﾐ､　　　　 　  , ﾐ }<br>" +
+    "　{   ﾐY ^^ 　 ^^ Yﾐ  }<br>" +
+    " Vﾐノ　　　　 　　ヽﾐ彡}<br>" +
+    " 彡′　　　　　 　　　`彡<br>" +
+    " 彡′ 　、　　　　 ,　　`彡<br>" +
+    " 彡  (fッ　　__　　fッ)　彡<br>" +
+    "彡　 　　 ¨Y¨　　　　 ﾐ<br>" +
+    "ﾐ　 　 　  ー'一　　　　ﾐ<br>" +
+    "ﾐ　　　　　　　　　　　　ﾐ<br>" +
+    "ﾐ　   ー ､ハハハハハr‐ イ  ﾐ<br>" +
+    "ﾐ　　　　　　　　　　ﾐ<br>" +
+    "ﾐ　　　　　　　　　　ﾐ<br>" +
+    "ﾐ　　　　　　　　　ﾐ";
+  const htmlSection4You = document.getElementById("section4-you");
+  const htmlSection4Opponent = document.getElementById("section4-opponent");
+  const htmlSection4Message = document.getElementById("section4-message");
+  htmlSection4You.innerHTML = "<p>" + yourPaca + "</p>";
+  htmlSection4Opponent.innerHTML = "<p>" + opponentPaca + "</p>";
+  htmlSection4Message.innerText = "If your alpaca's neck is longer than opposit, reduce 5 sec.";
+  return comment;
+}
+
+// Get your opponent alpaca's neck shorter as short as you can. 
+function alpacaAgain() {
+  const comment = "I recommend that" +
+    "in Windows, press 'ctrl' + 'k' to delete line" +
+    "in Mac, press 'command' + 'k' to delete line.";
+  const opponentPaca = "" +
+    " 　_　　　　　　　　_<br>" +
+    "　{ ﾐ､　　　　 　  , ﾐ }<br>" +
+    "　{   ﾐY ^^ 　 ^^ Yﾐ  }<br>" +
+    " Vﾐノ　　　　 　　ヽﾐ彡}<br>" +
+    " 彡′　　　　　 　　　`彡<br>" +
+    " 彡′ 　、　　　　 ,　　`彡<br>" +
+    " 彡  (fッ　　__　　fッ)　彡<br>" +
+    "彡　 　　 ¨Y¨　　　　 ﾐ<br>" +
+    "ﾐ　 　 　  ー'一　　　　ﾐ<br>" +
+    "ﾐ　　　　　　　　　　　　ﾐ<br>" +
+    "ﾐ　   ー ､ハハハハハr‐ イ  ﾐ<br>" +
+    "ﾐ　　　　　　　　　　ﾐ<br>" +
+    "ﾐ　　　　　　　　　　ﾐ<br>" +
+    "ﾐ　　　　　　　　　　ﾐ<br>" +
+    "ﾐ　　　　　　　　　　ﾐ<br>" +
+    "ﾐ　　　　　　　　　　ﾐ<br>" +
+    "ﾐ　　　　　　　　　　ﾐ<br>" +
+    "ﾐ　　　　　　　　　　ﾐ<br>" +
+    "ﾐ　　　　　　　　　　ﾐ<br>" +
+    "ﾐ　　　　　　　　　　ﾐ<br>" +
+    "ﾐ　　　　　　　　　　ﾐ<br>" +
+    "ﾐ　　　　　　　　　　ﾐ<br>" +
+    "ﾐ　　　　　　　　　　ﾐ<br>" +
+    "ﾐ　　　　　　　　　　ﾐ<br>" +
+    "ﾐ　　　　　　　　　　ﾐ<br>" +
+    "ﾐ　　　　　　　　　　ﾐ<br>" +
+    "ﾐ　　　　　　　　　　ﾐ<br>" +
+    "ﾐ　　　　　　　　　ﾐ";
+  const htmlSection5You = document.getElementById("section5-you");
+  const htmlSection5Opponent = document.getElementById("section5-opponent");
+  const htmlSection5Message = document.getElementById("section5-message");
+  htmlSection5You.innerHTML = "<p>" + yourPaca + "</p>";
+  htmlSection5Opponent.innerHTML = "<p>" + opponentPaca + "</p>";
+  htmlSection5Message.innerText = "If your alpaca's neck is longer than opposit, reduce 3 sec."
+  return comment;
+}
+
+function getRecord(){
+  const s = start.split(" ")[1] ? start.split(" ")[1] : [0,0,0];
+  const f = finish.split(" ")[1] ? finish.split(" ")[1] : [23,59,59];
+  const time = ((f[0] * 3600 || 23 * 3600) + (f[1] * 60 || 59 * 60) + (f[2] || 59))
+    - ((s[0] * 3600 || 0) + (s[1] * 60 || 0) + (s[2] || 0));
+  console.log("Your time: "+ time + " sec");
+}
+
+const foo = "font-size:48px;color:red;";
+const yourPaca = "" +
+  " 　_　　　　　　　　_<br>" +
+  "　{ ﾐ､　　　　 　  , ﾐ }<br>" +
+  "　{   ﾐY ^^ 　 ^^ Yﾐ  }<br>" +
+  " Vﾐノ　　　　 　　ヽﾐ彡}<br>" +
+  " 彡′　　　　　 　　　`彡<br>" +
+  " 彡′ 　、　　　　 ,　　`彡<br>" +
+  " 彡  (fッ　　__　　fッ)　彡<br>" +
+  "彡　 　　 ¨Y¨　　　　 ﾐ<br>" +
+  "ﾐ　 　 　  ー'一　　　　ﾐ<br>" +
+  "ﾐ　　　　　　　　　　　　ﾐ<br>" +
+  "ﾐ　   ー ､ハハハハハr‐ イ  ﾐ<br>" +
+  "ﾐ　　　　　　　　　　ﾐ<br>" +
+  "ﾐ　　　　　　　　　　ﾐ<br>" +
+  "ﾐ　　　　　　　　　　ﾐ<br>" +
+  "ﾐ　　　　　　　　　　ﾐ<br>" +
+  "ﾐ　　　　　　　　　ﾐ";
+const opponentPaca = yourPaca;
+
+function initializeDom(){
+  getRecord();
+  fixStatement();
+  setAttributes();
+  copyFromEOF();
+  alpaca();
+  alpacaAgain();
+}
+
+window.onload = initializeDom;
+
+// "/ ------\/ <br>" + 
